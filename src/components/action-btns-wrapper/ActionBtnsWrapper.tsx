be51410cf9b7892d9ef90d4371023761f09e5cca
danendz <danilapayne@gmail.com>
2025-03-18 08:50:21 +0500

    button fix and mobile navigation fix

--- src/components/action-btns-wrapper/ActionBtnsWrapper.tsx
+++ src/components/action-btns-wrapper/ActionBtnsWrapper.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 export default function ActionBtnsWrapper({children}: {children: ReactNode}) {
   return (
-    <div className="fixed primary-px right-0 mobile-nav-offset-b flex gap-3">
+    <div className="fixed primary-px mobile-nav-offset-b flex gap-3 container justify-end">
       {children}
     </div>
   )
--- src/components/navigation/MobileNavigation.tsx
+++ src/components/navigation/MobileNavigation.tsx
@@ -36,13 +36,17 @@ export const MobileNavigation = () => {
   const pathname = usePathname()
 
   return (
-    <nav className="flex justify-center fixed bottom-3 left-0 w-full primary-px">
-      <div className="flex border-1 rounded-xl container shadow-white/20 shadow-2xl bg-primary-foreground">
-        {items.map(({name, Icon, to}) => (
-          <Link key={name} href={to} className={clsx("flex-1 flex justify-center py-2", to === pathname ? 'text-primary' : 'text-muted-foreground')}>
-            <Icon/>
-          </Link>
-        ))}
+    <nav className="flex justify-center fixed bottom-3 left-0 w-full">
+      <div className="container primary-px">
+        <div
+          className="flex border-1 rounded-xl container shadow-white/20 shadow-2xl bg-primary-foreground">
+          {items.map(({name, Icon, to}) => (
+            <Link key={name} href={to}
+                  className={clsx("flex-1 flex justify-center py-2", to === pathname ? 'text-primary' : 'text-muted-foreground')}>
+              <Icon/>
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   )
